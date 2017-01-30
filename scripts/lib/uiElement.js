799682c3f100045af30ecd5580f9a4206e523676
ithenoob <david69rock@Hotmail.com>
2017-01-29 16:38:52 -0800

    Added some comments to UiElement

--- scripts/lib/uiElement.js
+++ scripts/lib/uiElement.js
@@ -3,17 +3,19 @@ define([], function() {
 
   var uniqueId = 0;
 
+  // Coordinates refer to pixel centers
+
   function UiElement(params) {
-    this.id = ++uniqueId;
-    this.name = params.name || "unnamed";
-    this.truePosition = params.truePosition || {left: 0, top: 0};
-    this.size = params.size || {left: 0, top: 0};
-    this.visible = params.visible || true;
-    this.children = [];
-    this.parent = params.parent || null;
-    this.verticalAlignment = params.verticalAlignment || 'top';
-    this.horizontalAlignment = params.horizontalAlignment || 'left';
-    this.position = params.position || {left: 0, top: 0};
+    this.id = ++uniqueId;                                             ///< Unique ID to this element
+    this.name = params.name || "unnamed";                             ///< None-unique user-friendly name
+    this.truePosition = params.truePosition || {left: 0, top: 0};     ///< Position of top left corner rel. parent's top left corner
+    this.size = params.size || {width: 0, height: 0};                 ///< This element's height
+    this.visible = params.visible || true;                            ///< This element's visibility
+    this.children = [];                                               ///< This element's children
+    this.parent = params.parent || null;                              ///< This element's parent
+    this.verticalAlignment = params.verticalAlignment || 'top';       ///< Vertical alignment: {top, center, bottom}
+    this.horizontalAlignment = params.horizontalAlignment || 'left';  ///< Horizontal alignment: {left, center, right}
+    this.position = params.position || {left: 0, top: 0};             ///< Offset from alignment-determined position
   }
 
   UiElement.prototype.show = function(bool) {
